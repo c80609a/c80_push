20e8334bb4117a0b3e416419f3c43876dd099100
C80609A <c080609a@gmail.com>
2017-06-26 15:23:43 +0300

    master: admin: Regions incoming.

---
+++ app/admin/c80_push/regions.rb
@@ -0,0 +1,35 @@
+ActiveAdmin.register C80Push::Region, as: 'Region' do
+
+  menu :label => proc{ I18n.t('c80_push.active_admin.menu.region')},
+       :parent => 'x_c80_push',
+       :priority => 2
+
+  permit_params :title,
+                :office_ids => [],
+                :dealer_ids => []
+
+  config.batch_actions = false
+  config.sort_order = 'id_asc'
+  before_filter :skip_sidebar!, :only => :index
+
+  index do
+    id_column
+    column :title
+    column :offices
+    column :dealers
+
+    actions
+  end
+
+  form(:html => {:multipart => true}) do |f|
+
+    f.inputs 'Свойства' do
+      f.input :title
+      f.input :offices, :as => :check_boxes
+      f.input :dealers, :as => :check_boxes
+    end
+
+    f.actions
+  end
+
+end
--- app/models/c80_push/region.rb
+++ app/models/c80_push/region.rb
@@ -2,5 +2,9 @@ module C80Push
   class Region < ActiveRecord::Base
     has_and_belongs_to_many :dealers
     has_and_belongs_to_many :offices
+    validates :title,
+              :uniqueness => true,
+              :presence => true,
+              :length => { in: 2..90 }
   end
 end
