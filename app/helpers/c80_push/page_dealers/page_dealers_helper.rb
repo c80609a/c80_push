cfc9b3448bf06a8fe409f6ffded8b5655ec760c0
C80609A <c080609a@gmail.com>
2017-06-27 18:48:03 +0300

    develop: рисую слева от карты список "Регионы-Дилеры-Офисы".

--- app/helpers/c80_push/page_dealers/page_dealers_helper.rb
+++ app/helpers/c80_push/page_dealers/page_dealers_helper.rb
@@ -2,8 +2,64 @@ module C80Push
   module PageDealers
     module PageDealersHelper
 
+      # Выдать список Дилеров (включая Офисы),
+      # разложенный по Регионам.
+      # Список выводится слева от карты.
+      #
+      # * Регион
+      #     * Дилер
+      #         * Офис 1
+      #             * адрес офиса
+      #             * телефон офиса
+      #         * Офис 2
+      #             * адрес офиса
+      #             * телефон офиса
+      #         * сайт дилера
+      #         * email дилера
+      #
       def render_dealers_list
-        
+
+        res = ''
+
+        Region.includes(dealers: :offices).each do |region|
+          r = region.title
+          r += ul_region_dealers(region)
+          res += "<li>#{r}</li>"
+        end
+
+        "<ul>#{res}</ul>".html_safe
+
+      end
+
+      private
+
+      def ul_region_dealers(region)
+        res = ''
+        region.dealers.each do |dealer|
+          d = dealer.title
+          d += ul_dealer_offices(dealer)
+          res += "<li>#{d}</li>"
+        end
+        "<ul>#{res}</ul>"#.html_safe
+      end
+
+      def ul_dealer_offices(dealer)
+        res = ''
+        dealer.offices.each do |office|
+          o = office.title
+          o += ul_office_props(office)
+          res += "<li>#{o}</li>"
+        end
+        res += "<li>#{dealer.email}</li>"
+        res += "<li>#{dealer.site}</li>"
+        "<ul>#{res}</ul>"#.html_safe
+      end
+
+      def ul_office_props(office)
+        res = ''
+        res += "<li>#{office.addr}</li>"
+        res += "<li>#{office.tel}</li>"
+        "<ul>#{res}</ul>"#.html_safe
       end
 
     end
