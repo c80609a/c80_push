dfc500dbe59902235ac4f79257e12f2f8545545a
C80609A <c080609a@gmail.com>
2017-06-27 18:54:19 +0300

    develop: добавил css-классы спискам "Регионы-Дилеры-Офисы".

--- app/helpers/c80_push/page_dealers/page_dealers_helper.rb
+++ app/helpers/c80_push/page_dealers/page_dealers_helper.rb
@@ -24,7 +24,7 @@ module C80Push
         Region.includes(dealers: :offices).each do |region|
           r = region.title
           r += ul_region_dealers(region)
-          res += "<li>#{r}</li>"
+          res += "<li id='region_#{region.id}'>#{r}</li>"
         end
 
         "<ul>#{res}</ul>".html_safe
@@ -33,33 +33,38 @@ module C80Push
 
       private
 
+      # используется только в render_dealers_list
       def ul_region_dealers(region)
         res = ''
+        # noinspection RubyResolve
         region.dealers.each do |dealer|
           d = dealer.title
           d += ul_dealer_offices(dealer)
-          res += "<li>#{d}</li>"
+          res += "<li id='dealer_#{dealer.id}'>#{d}</li>"
         end
-        "<ul>#{res}</ul>"#.html_safe
+        "<ul class='ul_region_dealers'>#{res}</ul>"#.html_safe
       end
 
+      # используется только в ul_region_dealers
       def ul_dealer_offices(dealer)
         res = ''
+        # noinspection RubyResolve
         dealer.offices.each do |office|
           o = office.title
           o += ul_office_props(office)
-          res += "<li>#{o}</li>"
+          res += "<li id='office_#{office.id}'>#{o}</li>"
         end
         res += "<li>#{dealer.email}</li>"
         res += "<li>#{dealer.site}</li>"
-        "<ul>#{res}</ul>"#.html_safe
+        "<ul class='ul_dealer_offices'>#{res}</ul>"#.html_safe
       end
 
+      # используется только в ul_dealer_offices
       def ul_office_props(office)
         res = ''
         res += "<li>#{office.addr}</li>"
         res += "<li>#{office.tel}</li>"
-        "<ul>#{res}</ul>"#.html_safe
+        "<ul class='ul_office_props'>#{res}</ul>"#.html_safe
       end
 
     end
