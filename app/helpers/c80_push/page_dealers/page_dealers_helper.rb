7a700761cf47225f7f54ee9d1957106b105785fc
C80609A <c080609a@gmail.com>
2017-06-27 19:17:00 +0300

    develop: обрамил все html-узлы списка "Регионы-Дилеры-Офисы" css-классами и айдишниками, добавил scss.

--- app/helpers/c80_push/page_dealers/page_dealers_helper.rb
+++ app/helpers/c80_push/page_dealers/page_dealers_helper.rb
@@ -17,17 +17,17 @@ module C80Push
       #         * сайт дилера
       #         * email дилера
       #
-      def render_dealers_list
+      def render_ul_dealers_list
 
         res = ''
 
         Region.includes(dealers: :offices).each do |region|
-          r = region.title
+          r = "<h2 class='region_title'>#{region.title}</h2>"
           r += ul_region_dealers(region)
-          res += "<li id='region_#{region.id}'>#{r}</li>"
+          res += "<li class='li_region' id='region_#{region.id}'>#{r}</li>"
         end
 
-        "<ul>#{res}</ul>".html_safe
+        "<ul class='ul_dealers_list'>#{res}</ul>".html_safe
 
       end
 
@@ -38,9 +38,9 @@ module C80Push
         res = ''
         # noinspection RubyResolve
         region.dealers.each do |dealer|
-          d = dealer.title
+          d = "<h3 class='dealer_title'>#{dealer.title}</h3>"
           d += ul_dealer_offices(dealer)
-          res += "<li id='dealer_#{dealer.id}'>#{d}</li>"
+          res += "<li class='li_dealer' id='dealer_#{dealer.id}'>#{d}</li>"
         end
         "<ul class='ul_region_dealers'>#{res}</ul>"#.html_safe
       end
@@ -50,20 +50,20 @@ module C80Push
         res = ''
         # noinspection RubyResolve
         dealer.offices.each do |office|
-          o = office.title
+          o = "<span class='office_title'>#{office.title}</span>"
           o += ul_office_props(office)
-          res += "<li id='office_#{office.id}'>#{o}</li>"
+          res += "<li class='li_office' id='office_#{office.id}'>#{o}</li>"
         end
-        res += "<li>#{dealer.email}</li>"
-        res += "<li>#{dealer.site}</li>"
+        res += "<li class='dealer_email'>#{dealer.email}</li>"
+        res += "<li class='dealer_site'>#{dealer.site}</li>"
         "<ul class='ul_dealer_offices'>#{res}</ul>"#.html_safe
       end
 
       # используется только в ul_dealer_offices
       def ul_office_props(office)
         res = ''
-        res += "<li>#{office.addr}</li>"
-        res += "<li>#{office.tel}</li>"
+        res += "<li class='office_addr'>#{office.addr}</li>"
+        res += "<li class='office_tel'>#{office.tel}</li>"
         "<ul class='ul_office_props'>#{res}</ul>"#.html_safe
       end
 
