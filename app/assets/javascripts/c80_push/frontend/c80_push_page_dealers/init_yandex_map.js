d05e20564a7c38e77ea957346a0e6ce56745da14
C80609A <c080609a@gmail.com>
2017-06-28 12:46:20 +0300

    develop: размещаю на карте точки всех офисов.

--- app/assets/javascripts/c80_push/frontend/c80_push_page_dealers/init_yandex_map.js
+++ app/assets/javascripts/c80_push/frontend/c80_push_page_dealers/init_yandex_map.js
@@ -6,12 +6,36 @@ $(document).ready(function() {
 
     //noinspection JSUnresolvedVariable
     ymaps.ready(function() {
+
         //noinspection JSUnresolvedVariable,JSUnresolvedFunction
         dealers_map = new ymaps.Map('dealers_map_container', {
             center: [55, 37],
             zoom: 10,
             controls: ['zoomControl']
         });
+
+        /*
+        // Создаем коллекцию геообъектов.
+        var myCollection = new ymaps.GeoObjectCollection();
+
+        // Добавляем метки в коллекцию.
+        myCollection.add(new ymaps.Placemark([37.61, 55.75]));
+        myCollection.add(new ymaps.Placemark([13.38, 52.51]));
+        myCollection.add(new ymaps.Placemark([30.30, 50.27]));
+
+        // Добавляем коллекцию на карту.
+        dealers_map.geoObjects.add(myCollection);
+
+        // Устанавливаем карте центр и масштаб так, чтобы охватить коллекцию целиком.
+        dealers_map.setBounds(myCollection.getBounds());
+        */
+
+        var c = new ymaps.GeoObjectCollection();
+        for (var i = 0; i < offices['all'].length; i++) {
+            c.add(new ymaps.Placemark(offices['all'][i]));
+        }
+        dealers_map.geoObjects.add(c);
+        dealers_map.setBounds(c.getBounds());
     });
 
 });
--- app/helpers/c80_push/application_helper.rb
+++ app/helpers/c80_push/application_helper.rb
@@ -10,10 +10,41 @@ module C80Push
       # список регионов, включая дилеры и офисы
       region_dealer_office_list = Region.includes(dealers: :offices)
 
+      ymap_hash = prepare_ymap_hash(region_dealer_office_list)
+
       render partial: 'c80_push/shared/page_dealers',
              locals: {
-                 rdo_list: region_dealer_office_list
+                 rdo_list: region_dealer_office_list,
+                 ymap_hash: ymap_hash
              }
+    end
+
+    private
+
+    # специально для яваскрипта, работающего с yandex картой,
+    # на основе +rdo+ собираем координаты всех офисов
+    # в хеше вида:
+    #
+    # {
+    #   all: []       - здесь собираем все офисы всех регионов
+    #   region_id: [] - данные офисов разложены по регионам
+    # }
+    #
+    def prepare_ymap_hash(rdo)
+      res = {all:[]}
+
+      rdo.each do |region|
+        region.dealers.each do |dealer|
+          dealer.offices.each do |office|
+            res[region.id] = [] if res[region.id].nil?
+            v = office.gps_arr
+            res[region.id] << v
+            res[:all] << v
+          end
+        end
+      end
+
+      res
     end
 
   end
